e5d6889bb1610db0fc9169e0746cf6a2274af817
Andy Linton <lintona@gmail.com>
2021-12-01 22:57:23 -0500

    Initial Commit for the Voyager Arduino code

---
+++ Voyager/Voyager.ino
@@ -0,0 +1,13 @@
+//Voyager Controller
+// This code handles the special effects for the Star Trek Voyager Scale Model.
+#include "VoyagerConfig.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Voyager/VoyagerConfig.h
@@ -0,0 +1,11 @@
+#ifndef VoyagerConfig
+#define VoyagerConfig
+
+#define NAV_LIGHTS_PIN 3
+#define NAV_BEACON_PIN 4
+#define HEAD_LIGHT_PIN 5
+#define PORT_PHASER_PIN 6
+#define STARBOARD_PHASER_PIN 7
+
+
+#endif
