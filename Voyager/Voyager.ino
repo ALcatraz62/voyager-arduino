28e9e947736453f98dd625b4e1ebb005c12f6771
Andy Linton <lintona@gmail.com>
2021-12-12 23:19:10 -0500

    toggle nav states via button input

--- Voyager/Voyager.ino
+++ Voyager/Voyager.ino
@@ -23,41 +23,65 @@ void loop()
   unsigned long currTime = millis();
   unsigned long delta = currTime - _lastUpdate;
 
+  _navBtn.update(currTime);
   updateNavState(delta);
   _lastUpdate = currTime;
 }
 
+/*
+ * NAV Lights are always on, but beacon blinks at a rate
+ * Nav Button cycles thru 3 modes OFF->BLINK->STEADY ON
+ */
 void updateNavState(unsigned long delta)
 {
   switch(_navState)
   {
     case OFF:
-    digitalWrite(NAV_LIGHTS_PIN, LOW);
-    digitalWrite(NAV_BEACON_PIN,LOW); 
-    break;
+      digitalWrite(NAV_LIGHTS_PIN, LOW);
+      digitalWrite(NAV_BEACON_PIN,LOW); 
+      if(_navBtn.isPressed())
+      {
+        _navCount = 0;
+        _navState = BLINK_ON;
+      }
+      break;
     case ON:
-    digitalWrite(NAV_LIGHTS_PIN, HIGH);
-    digitalWrite(NAV_BEACON_PIN, HIGH);
-    break;
+      digitalWrite(NAV_LIGHTS_PIN, HIGH);
+      digitalWrite(NAV_BEACON_PIN, HIGH);
+      if(_navBtn.isPressed())
+      {
+        _navState = OFF;
+      }
+      break;
     case BLINK_ON:
-    digitalWrite(NAV_LIGHTS_PIN,HIGH);
-    digitalWrite(NAV_BEACON_PIN,HIGH);
-    _navCount += delta;
-    if(_navCount >= BLINK_ON_TIME)
-    {
-      _navCount = 0;
-      _navState = BLINK_OFF;
-    }
-    break;
+      digitalWrite(NAV_LIGHTS_PIN,HIGH);
+      digitalWrite(NAV_BEACON_PIN,HIGH);
+      _navCount += delta;
+
+      if(_navBtn.isPressed())
+      {
+        _navState = OFF;
+      }
+      else if(_navCount >= BLINK_ON_TIME)
+      {
+        _navCount = 0;
+        _navState = BLINK_OFF;
+      }
+      break;
     case BLINK_OFF:
-    digitalWrite(NAV_LIGHTS_PIN,HIGH);
-    digitalWrite(NAV_BEACON_PIN,LOW);
-    _navCount += delta;
-    if(_navCount >= BLINK_OFF_TIME)
-    {
-      _navCount = 0;
-      _navState = BLINK_ON;
-    }
-    break;
+      digitalWrite(NAV_LIGHTS_PIN,HIGH);
+      digitalWrite(NAV_BEACON_PIN,LOW);
+      _navCount += delta;
+
+      if(_navBtn.isPressed())
+      {
+        _navState = OFF;
+      }
+      else if(_navCount >= BLINK_OFF_TIME)
+      {
+        _navCount = 0;
+        _navState = BLINK_ON;
+      }
+      break;
   }
 }
