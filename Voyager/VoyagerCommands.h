b4402bc3dd09cb67a5045249bf972cecb4acdec3
Andy Linton <lintona@gmail.com>
2022-04-07 07:27:32 -0400

    Added command constants file

---
+++ Voyager/VoyagerCommands.h
@@ -0,0 +1,13 @@
+#ifndef VoyagerCommands
+#define VoyagerCommands
+  //Command Strings
+  #define NAV_CMD "NAV"
+  #define HEADLIGHT_CMD "HEAD"
+  #define DEFLECTOR_CMD "DEFLECT"
+  #define INTERIOR_CMD "INTER"
+  #define WARP_CMD "WARP"
+  #define PHOTON_CMD "PHO"
+  #define FOWARD_FIRE_CMD "FWD"
+  #define AFT_FIRE_CMD "AFT"
+  #define STATUS_TEXT_CMD "STATUS"
+#endif
